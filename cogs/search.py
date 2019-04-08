a55072565ae94df8f0e8c2d5487b64880be7546f
Andrew <lee-a5@email.ulster.ac.uk>
2019-04-08 18:52:56 +0100

    AniList Manga

--- cogs/search.py
+++ cogs/search.py
@@ -167,6 +167,75 @@ class search:
                 )
             )
 
+    @commands.command()
+    async def manga(self, *, text: str):
+        """Search AniList for manga"""
+        query = """
+        query ($search: String) {
+            Media (search: $search, type: MANGA) {
+                id
+                title {
+                    romaji
+                    english
+                    native
+                }
+                description
+                chapters
+                volumes
+                status
+                genres
+                averageScore
+                coverImage {
+                    large
+                }
+            }
+        }
+        """
+
+        variables = {"search": text}
+
+        response = requests.post(
+            "https://graphql.anilist.co", json={"query": query, "variables": variables}
+        )
+
+        rateLimitRemaining = int(response.headers["X-RateLimit-Remaining"])
+
+        # Rate limiting is currently set to 90 requests per minute
+        # If you go over the rate limit you'll receive a 1-minute timeout
+        # https://anilist.gitbook.io/anilist-apiv2-docs/overview/rate-limiting
+        if rateLimitRemaining > 0:
+            mangaJSON = response.json()["data"]["Media"]
+
+            description = mangaJSON["description"]
+            description = description.replace("<br>", "")
+            genres = ""
+            for g in mangaJSON["genres"]:
+                genres += "{}, ".format(g)
+
+            embed = discord.Embed(
+                title="{} / {}".format(
+                    mangaJSON["title"]["romaji"], mangaJSON["title"]["native"]
+                ),
+                url="https://anilist.co/manga/{}".format(mangaJSON["id"]),
+                description=description,
+            )
+            embed.set_thumbnail(url=mangaJSON["coverImage"]["large"])
+            embed.add_field(name="Chapters", value=mangaJSON["chapters"], inline=True)
+            embed.add_field(name="Volumes", value=mangaJSON["volumes"], inline=True)
+            embed.add_field(name="Status", value=mangaJSON["status"], inline=True)
+            embed.add_field(name="Genres", value=genres[:-2], inline=True)
+            embed.add_field(
+                name="Average Score", value=mangaJSON["averageScore"], inline=True
+            )
+            embed.set_footer(text="Powered by anilist.co")
+            await self.bot.say(embed=embed)
+        else:
+            await self.bot.say(
+                "The bot is currently being rate limited :( Try again in {} seconds".format(
+                    response.headers["Retry-After"]
+                )
+            )
+
 
 def setup(bot):
     bot.add_cog(search(bot))
