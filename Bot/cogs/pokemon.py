80706d5d2af5adb30b54c32f55f6b45e14695c39
andrewlee91 <lee-a5@email.ulster.ac.uk>
2019-07-12 00:13:26 +0100

    Search Pokemon type

--- Bot/cogs/pokemon.py
+++ Bot/cogs/pokemon.py
@@ -14,36 +14,98 @@ class pokemon(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def pokemon(self, ctx, text: str):
+    @commands.group()
+    async def pokemon(self, ctx):
         """Search for a Pokemon by name or id"""
-        req = requests.get("https://pokeapi.co/api/v2/pokemon/{}".format(text))
+        if ctx.invoked_subcommand is None:
+            # For some reason I couldn't get subcommands to work when I passed an extra argument like I normally do
+            # so I'm just extracting it from the message context instead even if it looks ugly
+            text = ctx.message.content.split()[1]
+
+            req = requests.get("https://pokeapi.co/api/v2/pokemon/{}".format(text))
+            data = req.json()
+            req = requests.get(data["species"]["url"])
+            species_data = req.json()
+
+            name = data["name"].capitalize()
+            pokemon_id = data["id"]
+            rand = random.randint(0, 450)
+            if rand == 450:
+                thumbnail = data["sprites"]["front_shiny"]
+            else:
+                thumbnail = data["sprites"]["front_default"]
+
+            for temp in species_data["flavor_text_entries"]:
+                if temp["language"]["name"] == "en":
+                    flavor_text = temp["flavor_text"]
+                    break
+
+            embedMessage = discord.Embed(title=flavor_text, colour=0x87CEEB)
+            embedMessage.set_author(name="{}\t\t#{}".format(name, pokemon_id))
+            embedMessage.set_thumbnail(url=thumbnail)
+
+            for stat in data["stats"]:
+                embedMessage.add_field(
+                    name=stat["stat"]["name"], value=stat["base_stat"], inline=True
+                )
+
+            embedMessage.set_footer(text="All data from https://pokeapi.co/")
+
+            await ctx.send(embed=embedMessage)
+
+    @pokemon.command()
+    async def type(self, ctx, text: str):
+        """Search for a Pokemon type"""
+        req = requests.get("https://pokeapi.co/api/v2/type/{}".format(text))
         data = req.json()
-        req = requests.get(data["species"]["url"])
-        species_data = req.json()
 
-        name = data["name"].capitalize()
-        pokemon_id = data["id"]
-        rand = random.randint(0, 450)
-        if rand == 450:
-            thumbnail = data["sprites"]["front_shiny"]
+        damage_type = data["move_damage_class"]["name"].capitalize()
+
+        temp = []
+        for t in data["damage_relations"]["double_damage_from"]:
+            temp.append(t["name"].capitalize())
+        double_damage_from = ", ".join(temp)
+
+        temp = []
+        for t in data["damage_relations"]["double_damage_to"]:
+            temp.append(t["name"].capitalize())
+        double_damage_to = ", ".join(temp)
+
+        temp = []
+        for t in data["damage_relations"]["half_damage_from"]:
+            temp.append(t["name"].capitalize())
+        half_damage_from = ", ".join(temp)
+
+        temp = []
+        for t in data["damage_relations"]["half_damage_to"]:
+            temp.append(t["name"].capitalize())
+        half_damage_to = ", ".join(temp)
+
+        if data["damage_relations"]["no_damage_from"]:
+            no_damage_from = data["damage_relations"]["no_damage_from"]
         else:
-            thumbnail = data["sprites"]["front_default"]
+            no_damage_from = "None"
+        if data["damage_relations"]["no_damage_to"]:
+            no_damage_to = data["damage_relations"]["no_damage_to"]
+        else:
+            no_damage_to = "None"
 
-        for temp in species_data["flavor_text_entries"]:
-            if temp["language"]["name"] == "en":
-                flavor_text = temp["flavor_text"]
-                break
-
-        embedMessage = discord.Embed(title=flavor_text, colour=0x87CEEB)
-        embedMessage.set_author(name="{}\t\t#{}".format(name, pokemon_id))
-        embedMessage.set_thumbnail(url=thumbnail)
-
-        for stat in data["stats"]:
-            embedMessage.add_field(
-                name=stat["stat"]["name"], value=stat["base_stat"], inline=True
-            )
-
+        embedMessage = discord.Embed(
+            title="Attack type: {}".format(damage_type), colour=0x87CEEB
+        )
+        embedMessage.set_author(name=text.capitalize())
+        embedMessage.add_field(
+            name="Double Damage To", value=double_damage_to, inline=True
+        )
+        embedMessage.add_field(
+            name="Double Damage From", value=double_damage_from, inline=True
+        )
+        embedMessage.add_field(name="Half Damage To", value=half_damage_to, inline=True)
+        embedMessage.add_field(
+            name="Half Damage From", value=half_damage_from, inline=True
+        )
+        embedMessage.add_field(name="No Damage To", value=no_damage_to, inline=True)
+        embedMessage.add_field(name="No Damage From", value=no_damage_from, inline=True)
         embedMessage.set_footer(text="All data from https://pokeapi.co/")
 
         await ctx.send(embed=embedMessage)
