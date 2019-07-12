c3088c01f2366744a6b754ff83b649dc0e3a61bd
andrewlee91 <lee-a5@email.ulster.ac.uk>
2019-07-12 16:59:12 +0100

    Search Pokemon abilities
    And also some formatting clean up

--- Bot/cogs/pokemon.py
+++ Bot/cogs/pokemon.py
@@ -40,14 +40,39 @@ class pokemon(commands.Cog):
                     flavor_text = temp["flavor_text"]
                     break
 
+            temp = []
+            for t in data["types"]:
+                temp.append(t["type"]["name"].capitalize())
+            pokemon_type = ", ".join(temp)
+
+            hp = data["stats"][5]["base_stat"]
+            attack = data["stats"][4]["base_stat"]
+            special_attack = data["stats"][2]["base_stat"]
+            defence = data["stats"][3]["base_stat"]
+            special_defence = data["stats"][1]["base_stat"]
+            speed = data["stats"][0]["base_stat"]
+
+            temp = []
+            for t in data["abilities"]:
+                temp.append(t["ability"]["name"].capitalize())
+            abilities = ", ".join(temp)
+
             embedMessage = discord.Embed(title=flavor_text, colour=0x87CEEB)
-            embedMessage.set_author(name="{}\t\t#{}".format(name, pokemon_id))
+            embedMessage.set_author(name="#{} - {}".format(pokemon_id, name))
             embedMessage.set_thumbnail(url=thumbnail)
 
-            for stat in data["stats"]:
-                embedMessage.add_field(
-                    name=stat["stat"]["name"], value=stat["base_stat"], inline=True
-                )
+            embedMessage.add_field(name="Type", value=pokemon_type, inline=False)
+            embedMessage.add_field(name="HP", value=hp, inline=True)
+            embedMessage.add_field(name="Attack", value=attack, inline=True)
+            embedMessage.add_field(
+                name="Special Attack", value=special_attack, inline=True
+            )
+            embedMessage.add_field(name="Defence", value=defence, inline=True)
+            embedMessage.add_field(
+                name="Special Defence", value=special_defence, inline=True
+            )
+            embedMessage.add_field(name="Speed", value=speed, inline=True)
+            embedMessage.add_field(name="Abilities", value=abilities, inline=False)
 
             embedMessage.set_footer(text="All data from https://pokeapi.co/")
 
@@ -81,12 +106,19 @@ class pokemon(commands.Cog):
             temp.append(t["name"].capitalize())
         half_damage_to = ", ".join(temp)
 
-        if data["damage_relations"]["no_damage_from"]:
-            no_damage_from = data["damage_relations"]["no_damage_from"]
+        if data["damage_relations"]["no_damage_from"]:#
+            temp = []
+            for t in data["damage_relations"]["no_damage_from"]:
+                temp.append(t["name"].capitalize())
+            no_damage_from = ", ".join(temp)
         else:
             no_damage_from = "None"
+
         if data["damage_relations"]["no_damage_to"]:
-            no_damage_to = data["damage_relations"]["no_damage_to"]
+            temp = []
+            for t in data["damage_relations"]["no_damage_to"]:
+                temp.append(t["name"].capitalize())
+            no_damage_to = ", ".join(temp)
         else:
             no_damage_to = "None"
 
@@ -94,6 +126,7 @@ class pokemon(commands.Cog):
             title="Attack type: {}".format(damage_type), colour=0x87CEEB
         )
         embedMessage.set_author(name=text.capitalize())
+
         embedMessage.add_field(
             name="Double Damage To", value=double_damage_to, inline=True
         )
@@ -106,6 +139,23 @@ class pokemon(commands.Cog):
         )
         embedMessage.add_field(name="No Damage To", value=no_damage_to, inline=True)
         embedMessage.add_field(name="No Damage From", value=no_damage_from, inline=True)
+
+        embedMessage.set_footer(text="All data from https://pokeapi.co/")
+
+        await ctx.send(embed=embedMessage)
+
+    @pokemon.command()
+    async def ability(self, ctx, text: str):
+        """Search for a Pokemon ability"""
+        req = requests.get("https://pokeapi.co/api/v2/ability/{}".format(text.lower()))
+        data = req.json()
+
+        name = data["name"].capitalize()
+        description = data["effect_entries"][0]["effect"]
+
+        embedMessage = discord.Embed(title=description, colour=0x87CEEB)
+        embedMessage.set_author(name=name)
+
         embedMessage.set_footer(text="All data from https://pokeapi.co/")
 
         await ctx.send(embed=embedMessage)
